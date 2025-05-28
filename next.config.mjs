0a54b1759ef1ec6629637c96d277c69712d3b5a5
unknown <tomerpacific@gmail.com>
2025-05-28 20:36:53 +0300

    MediumArticlesPortfolio adding prefix path for assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  assetPrefix: isProd ? '/medium-articles-portfolio/' : '',
+  basePath: isProd ? '/medium-articles-portfolio' : '',
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
