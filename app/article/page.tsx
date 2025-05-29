8b84fc9b48cd4ca4e7e587d601700cb45c1324c9
unknown <tomerpacific@gmail.com>
2025-05-29 21:31:32 +0300

    MediumArticlesPortfolio using navigation instead of router

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
 import { CalendarIcon } from "lucide-react"
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 export const articles = [
   {
